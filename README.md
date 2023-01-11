b91fdfa6b12a3b49500c3a30d16ceb7a9a256318
Ashley Sese <sese.ashrean@gmail.com>
2023-01-10 23:23:58 -0800

    finished pseudocode and working on html

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Check City Weather
 
+# Table of Contents
+- Description
+- Installation Instructions
+- GitHub Account
+- Contacts
+- Code Snippets
+- Resources
+- Credits
+
+
+# Descriptiom
+
+# Installtion Instructions
+
+# Github Account
+
+# Contacts
+Email - sese.ashrean@gmail.com
+
+Linkedin - https://www.linkedin.com/in/ashleyrean/
+
+# Code Snippets
+
+# Resources
+
+# Credits
---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Pseudocode
+
+// Base URL
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={9ddc12efb6a46b86c5ae9bd300cf132d}
+
+// Search City Section
+    // Create a input text area for the user to type in the city they are searching for
+        // The input text are must have a search button
+
+    // When the user clicks on the search button then that city is then saved to the page
+        // Create a clear button to delete all the saved cities and the selected city
+
+
+// Displaying Conditions on Selected City
+    // When city has been selected the following criteria must be dispalyed
+    // Criteria:
+        // City's Name
+        // Date
+        // Conditions
+            // current and future
+        // Icon displaying the conditions
+        // Display the upcoming 5day forecast
